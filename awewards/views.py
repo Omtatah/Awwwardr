a03c3c6b1c62254717630d8f7ecabc3e98b0b7bb
Omtatah <murtallahomtatah@gmail.com>
2019-08-04 16:30:18 +0300

    define the register view

--- awewards/views.py
+++ awewards/views.py
@@ -5,3 +5,12 @@ from django.contrib.auth.decorators import login_required
 from .forms import *
 from django.http import Http404
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', locals())
