60fc421ebbf39c68cf2689582ced01480abac3cc
Omtatah <murtallahomtatah@gmail.com>
2019-08-04 17:05:33 +0300

    properly indent the website

--- awwwards/urls.py
+++ awwwards/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
@@ -24,13 +24,14 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^',include('awewards.urls')),
+    url(r'^', include('awewards.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
-    url(r'^ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
+    url(r'^ratings/', include('star_ratings.urls',
+                              namespace='ratings', app_name='ratings')),
     url(r'^api-token-auth/', obtain_auth_token),
     url(r'^search/$', views.search, name='search'),
-
 ]
 
 if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
